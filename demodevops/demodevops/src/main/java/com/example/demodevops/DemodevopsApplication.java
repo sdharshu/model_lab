799c0ad40f160c0f7875db3fd4a6890bd3015675
DHARSHINI S <167686298+sdharshu@users.noreply.github.com>
2025-04-15 11:39:55 +0530

    Add files via upload

---
+++ demodevops/demodevops/src/main/java/com/example/demodevops/DemodevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodevopsApplication.class, args);
+	}
+
+}
